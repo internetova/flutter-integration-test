e0cca408dcd9387ae78d31e7d8c91e2b679d598a
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-03-19 21:11:56 +0300

    Ключи для тестов

---
+++ lib/utils/warming_up_keys.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Ключи для интеграционных тестов для прогрева шейдеров
+class WarmingUpKeys {
+  static const appBarTitleLoginKey = ValueKey('appBarTitleLoginKey');
+  static const appBarTitleLogoutKey = ValueKey('appBarTitleLogoutKey');
+  static const alert = ValueKey('Alert');
+  static const cancelAlert = ValueKey('CancelAlert');
+  static const confirmAlert = ValueKey('ConfirmAlert');
+  static const loginBtn = ValueKey('Войти');
+  static const logoutBtn = ValueKey('Выйти');
+}
